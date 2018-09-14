4df190ed08adeab18cb8e114374bff0b4b2ea3b7
roni46 <39713502+roni46@users.noreply.github.com>
2018-09-15 00:42:56 +0700

    Delete hapus.php

--- prauas/hapus.php
+++
@@ -1,9 +0,0 @@
-<?php
-include_once 'koneksi.php';
-
-
-$id = $_GET['id'];
-$sql = "DELETE FROM artikel WHERE  id  = '{$id}'";
-$result = mysqli_query($conn, $sql);
-header('location: artikel.php');
-?>
